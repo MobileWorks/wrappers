ead339cee0901e1e853e9641a10af7f3d5d88837
Mouad Debbar <mouad.debbar@gmail.com>
2012-03-25 13:42:17 -0700

    Python: package is ready.

---
+++ python/MobileWorks/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup( 
+    name = 'MobileWorks',
+    version = '0.1.0',
+    author = 'Mouad Debbar',
+    author_email = 'mouad@mobileworks.com',
+    py_modules = ['mobileworks'],
+#    url = 'http://pypi.python.org/pypi/TowelStuff/',
+#    license = 'LICENSE.txt',
+    description = 'A wrapper for the MobileWorks API.',
+#    long_description = open( 'README.txt' ).read(),
+ )
