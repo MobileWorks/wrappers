2f6993cea68d58da3f88850757112c4595bc7798
Mouad Debbar <mouad.debbar@gmail.com>
2012-03-26 11:18:55 -0700

    PHP: CamelCase -> underscores, `make_request` is now private.

--- php/mobileworks.php
+++ php/mobileworks.php
@@ -18,15 +18,15 @@ class MobileWorks {
 	 * Creates and sends a request to the specified $url using the specified $method.
 	 * @param string $url The URL of the request.
 	 * @param string $method The method of the request (GET/POST/DELETE).
-	 * @param null|string|array $postData  The data to be posted (optional).
+	 * @param null|string|array $post_data  The data to be posted (optional).
 	 * @return array The array representation of the returned JSON object.
 	 * @throws Exception
 	 */
-	function makeRequest( $url, $method = 'GET', $postData = null ) {
+	private function make_request( $url, $method = 'GET', $post_data = null ) {
 		$req = curl_init( $url );
 		curl_setopt( $req, CURLOPT_CUSTOMREQUEST, $method );
-		if ( $method == 'POST' && !is_null( $postData ) ) {
-			curl_setopt( $req, CURLOPT_POSTFIELDS, $postData );
+		if ( $method == 'POST' && !is_null( $post_data ) ) {
+			curl_setopt( $req, CURLOPT_POSTFIELDS, $post_data );
 		}
 		curl_setopt( $req, CURLOPT_RETURNTRANSFER, true );
 		curl_setopt( $req, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
@@ -37,72 +37,72 @@ class MobileWorks {
 			curl_close( $req );
 			throw new Exception( curl_error( $req ) );
 		}
-		$httpCode = curl_getinfo( $req, CURLINFO_HTTP_CODE );
-		echo $httpCode, PHP_EOL;
+		$http_code = curl_getinfo( $req, CURLINFO_HTTP_CODE );
+		echo $http_code, PHP_EOL;
 		curl_close( $req );
-		if ( $httpCode >= 500 ) {
-			throw new Exception( "HTTP $httpCode: A server error occured" );
+		if ( $http_code >= 500 ) {
+			throw new Exception( "HTTP $http_code: A server error occured" );
 		}
-		if ( $httpCode >= 300 ) {
-			throw new Exception( "HTTP $httpCode: $result" );
+		if ( $http_code >= 300 ) {
+			throw new Exception( "HTTP $http_code: $result" );
 		}
 		return get_object_vars( json_decode( $result ) );
 	}
 
 	/**
 	 * Posts a task to MobileWorks and returns the URL of the created task.
-	 * @param array $taskParams The parameters of the task to be created.
+	 * @param array $task_params The parameters of the task to be created.
 	 * @return string The URL of the created task.
 	 */
-	function postTask( $taskParams ) {
-		$response = $this->makeRequest( $this->task_url, 'POST', json_encode( $taskParams ) );
+	function postTask( $task_params ) {
+		$response = $this->make_request( $this->task_url, 'POST', json_encode( $task_params ) );
 		return $response['Location'];
 	}
 
 	/**
 	 * Gets the information of the task located in $taskUrl.
-	 * @param string $taskUrl The URL of the task to be retrieved.
+	 * @param string $task_url The URL of the task to be retrieved.
 	 * @return array The task's information.
 	 */
-	function retrieveTask( $taskUrl ) {
-		return $this->makeRequest( $taskUrl );
+	function retrieveTask( $task_url ) {
+		return $this->make_request( $task_url );
 	}
 
 	/**
 	 * Deletes the task located in $taskUrl.
-	 * @param string $taskUrl The URL of the task to be deleted.
+	 * @param string $task_url The URL of the task to be deleted.
 	 * @return array
 	 */
-	function deleteTask( $taskUrl ) {
-		return $this->makeRequest( $taskUrl, 'DELETE' );
+	function deleteTask( $task_url ) {
+		return $this->make_request( $task_url, 'DELETE' );
 	}
 
 	/**
 	 * Posts a job to MobileWorks and returns the URL of the created job.
-	 * @param array $jobParams The parameters of the job to be created.
+	 * @param array $job_params The parameters of the job to be created.
 	 * @return string The URL of the created job.
 	 */
-	function postJob( $jobParams ) {
-		$response = $this->makeRequest( $this->job_url, 'POST', json_encode( $jobParams ) );
+	function postJob( $job_params ) {
+		$response = $this->make_request( $this->job_url, 'POST', json_encode( $job_params ) );
 		return $response['Location'];
 	}
 
 	/**
 	 * Gets the information of the job located in $jobUrl.
-	 * @param string $jobUrl The URL of the job to be retrieved.
+	 * @param string $job_url The URL of the job to be retrieved.
 	 * @return array The job's information.
 	 */
-	function retrieveJob( $jobUrl ) {
-		return $this->makeRequest( $jobUrl );
+	function retrieveJob( $job_url ) {
+		return $this->make_request( $job_url );
 	}
 
 	/**
 	 * Deletes the job located in $jobUrl.
-	 * @param string $jobUrl The URL of the job to be deleted.
+	 * @param string $job_url The URL of the job to be deleted.
 	 * @return array
 	 */
-	function deleteJob( $jobUrl ) {
-		return $this->makeRequest( $jobUrl, 'DELETE' );
+	function deleteJob( $job_url ) {
+		return $this->make_request( $job_url, 'DELETE' );
 	}
 
 }
