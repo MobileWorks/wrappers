f4b8ec532d8eb0a8bacde911d163812d22f28727
Mouad Debbar <mouad.debbar@gmail.com>
2012-04-05 15:01:23 -0700

    Python: Added Task and Job classes. Added the login functionality through the login API.

--- python/MobileWorks/setup.py
+++ python/MobileWorks/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup( 
     name = 'MobileWorks',
-    version = '0.1.4',
+    version = '1.0.0',
     py_modules = ['mobileworks'],
     url = 'http://www.mobileworks.com/',
     description = 'A wrapper for the MobileWorks API.',
